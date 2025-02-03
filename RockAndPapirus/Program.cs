8bc05f50ce8671ca6a442ad42e7075bd7c4a3fe1
Daniil <Daniilmiadzve@gmail.com>
2025-02-03 01:13:34 +0100

    ADD

---
+++ RockAndPapirus/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("hello");
+       Console.WriteLine("Make a choise");
+       Console.WriteLine("1-Rock" + " " + "- 2- scissors" + " " + " 3 - Paper");
+       
+       Random rnd = new Random();
+       
+       //генерируем число для компьютера 
+       // парсим пользвательский ввод
+        string b = Console.ReadLine();
+        int userChoice = Int32.Parse(b);
+      
+        int computerChoice = rnd.Next(1, 4);
+        
+        
+      
+        while (true)
+        {
+            if (userChoice == computerChoice)
+            {
+                Console.WriteLine("Draw!");
+            }
+            else if (userChoice == 1 && computerChoice == 2 || userChoice == 2 && computerChoice == 3
+                                                            || userChoice == 3 && computerChoice == 1)
+            {
+                Console.WriteLine("you won");
+            }
+            else
+            {
+                Console.WriteLine("you lost" + " " + "Computer win!");
+            }
+
+            string g = Console.ReadLine();
+            if (g != "9")
+            {
+              break;
+            }
+
+            Console.WriteLine("Thank you for playing!");
+        }
+        
+    }
+}
